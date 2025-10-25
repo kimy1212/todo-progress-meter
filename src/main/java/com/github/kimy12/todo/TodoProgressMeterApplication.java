4e5b39ef27a13d5c9e36eaf0226d395f421555ab
Kimy-12 <yuki6530y6234@gmail.com>
2025-10-25 22:35:45 +0900

    first commit

---
+++ src/main/java/com/github/kimy12/todo/TodoProgressMeterApplication.java
@@ -0,0 +1,13 @@
+package com.github.kimy12.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoProgressMeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoProgressMeterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/kimy12/TodoProgressMeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kimy12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoProgressMeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
