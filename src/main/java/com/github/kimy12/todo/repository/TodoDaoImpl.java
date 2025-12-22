c922fd6d83008466059a634bf83f8d900808d322
Kimy-12 <yuki6530y6234@gmail.com>
2025-12-22 23:58:31 +0900

    refactor: replace field injection with constructor injection

--- src/main/java/com/github/kimy12/todo/repository/TodoDaoImpl.java
+++ src/main/java/com/github/kimy12/todo/repository/TodoDaoImpl.java
@@ -3,19 +3,21 @@ package com.github.kimy12.todo.repository;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import com.github.kimy12.todo.dto.TodoTab;
 import com.github.kimy12.todo.dto.Todo;
+import com.github.kimy12.todo.dto.TodoTab;
 
 @Repository
 public class TodoDaoImpl implements TodoDao {
 
-	@Autowired
-	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+	private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
+	public TodoDaoImpl(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+		this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+	}
+	
 	@Override
 	public List<TodoTab> getTabsByUser(final String userId) {
 		String sql = """
